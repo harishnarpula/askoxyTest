dc9e74dbade5a3ba2e06a828d4b1c75a6586780d
Haribabu <harishnarpula1121@gmail.com>
2026-04-12 14:06:14 +0530

    only code

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,5 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import Finvibe3DLanding from "./Finvibe/Finvibe3DLanding";
-import Landing from "./Finvibe/Landing";
-import CodeExplorerPage from "./Finvibe/components/CodeExplorerPage";
-
+import CodeExplorer from "./Finvibe/components/CodeExplorer";
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Finvibe3DLanding />} />
-      <Route path="/finvibe-code-builder" element={<Landing />} />
-      <Route path="/insurvibe-code-builder" element={<Landing />} />
-      <Route path="/bfsi-projects" element={<CodeExplorerPage />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
-  );
+  return <CodeExplorer />;
 }
